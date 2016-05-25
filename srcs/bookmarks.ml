37651011d8950eab5de94a8a8b37fcc87a033ce8
jaguillo <juloo.dsi@gmail.com>
2016-05-25 20:07:16 +0200

    More test on receiver

--- srcs/bookmarks.ml
+++ srcs/bookmarks.ml
@@ -6,7 +6,7 @@
 (*   By: jaguillo <jaguillo@student.42.fr>          +#+  +:+       +#+        *)
 (*                                                +#+#+#+#+#+   +#+           *)
 (*   Created: 2016/05/24 12:30:10 by jaguillo          #+#    #+#             *)
-(*   Updated: 2016/05/24 19:26:50 by jaguillo         ###   ########.fr       *)
+(*   Updated: 2016/05/25 19:09:00 by jaguillo         ###   ########.fr       *)
 (*                                                                            *)
 (* ************************************************************************** *)
 
@@ -22,13 +22,13 @@ type t = {
 
 }
 
-type map_t = t Js_dict.t
+module OnClickReceiver = Receiver.Make (struct type t = Js.js_string Js.t end)
 
-let rec create_bookmark dict parent_element node =
+let rec create dict parent_element node =
 	let element =
 		let div = Dom_html.createDiv Dom_html.document in
 		let a = Dom_html.createA Dom_html.document in
-		let onClick _ = Js._false in
+		let onClick _ = OnClickReceiver.transmit node##id; Js._false in
 		a##textContent <- (Js.some node##title);
 		Js.Optdef.case (node##url) (fun _ -> ()) (fun url -> a##href <- url);
 		a##classList##add (Js.string "bookmark_label");
@@ -38,6 +38,7 @@ let rec create_bookmark dict parent_element node =
 		div##classList##add (Js.string (if Js.Optdef.test (node##url) then "bookmark" else "bookmark_folder"));
 		div
 	in
+
 	Js_dict.put dict (node##id) {
 		opened = false;
 		id = node##id;
@@ -45,11 +46,14 @@ let rec create_bookmark dict parent_element node =
 		element = element;
 		childs = Js.Optdef.case (node##children)
 			(fun () -> [||])
-			(fun c -> Array.map (create_bookmark dict element) (Js.to_array c))
+			(fun c -> Array.map (create dict element) (Js.to_array c))
 	};
 	node##id
 
-let create_map parent_element tree =
-	let dict = Js_dict.create () in
-	Array.iter (fun node -> ignore (create_bookmark dict parent_element node)) (Js.to_array tree);
-	dict
+let set_opened b v =
+	let open_class = Js.string "open" in
+	b.opened <- v;
+	if v then
+		b.element##classList##add (open_class)
+	else
+		b.element##classList##remove (open_class)
--- srcs/main.ml
+++ srcs/main.ml
@@ -6,30 +6,43 @@
 (*   By: jaguillo <jaguillo@student.42.fr>          +#+  +:+       +#+        *)
 (*                                                +#+#+#+#+#+   +#+           *)
 (*   Created: 2016/05/24 19:10:12 by jaguillo          #+#    #+#             *)
-(*   Updated: 2016/05/25 00:10:02 by juloo            ###   ########.fr       *)
+(*   Updated: 2016/05/25 20:05:10 by jaguillo         ###   ########.fr       *)
 (*                                                                            *)
 (* ************************************************************************** *)
 
 let log s = Js.Unsafe.fun_call (Js.Unsafe.js_expr "console.log") [| Js.Unsafe.inject s |]
 
-module TestTest = Test.Make (struct type t = int end)
+module KeyboardReceiver = Receiver.Make (struct type t = unit end)
+
+type receiver_t = Bookmark_click of Bookmarks.OnClickReceiver.t | Keyboard_event
+
+module RootReceiver = Receiver.Join (Bookmarks.OnClickReceiver) (KeyboardReceiver)
+	(struct
+		type t = receiver_t
+		let left v = Bookmark_click v
+		let right () = Keyboard_event
+	end)
 
 let () =
+	let bookmarks = Js_dict.create () in
 
-	TestTest.register (fun event ->
-			(* Main loop *)
-			log event
-		);
+	RootReceiver.bind (function
+		| Bookmark_click (b)	->
+			let b = Js_dict.get bookmarks b in
+			Js.Optdef.case b
+				(fun () -> assert false)
+				(fun b -> Bookmarks.set_opened b (not b.Bookmarks.opened))
+		| Keyboard_event		->
+			log (Js.string "KEYBOARD")
+	);
 
-	(* Register to async callbacks *)
+	let callback tree =
+		let iter node =
+			ignore (Bookmarks.create bookmarks (Dom_html.document##body) node)
+		in
+		Array.iter iter (Js.to_array tree)
+	in
+	Chrome_bookmarks.getTree callback;
 
-	(* Test *)
-	TestTest.push 4;
-	TestTest.push 5;
-	TestTest.push 1;
-	TestTest.push 9;
-
-	();
-
-	let callback tree = ignore (Bookmarks.create_map (Dom_html.document##body) tree) in
-	Chrome_bookmarks.getTree callback
+	ignore (Dom_html.addEventListener Dom_html.document (Dom_html.Event.keypress)
+		(Dom_html.handler (fun _ -> KeyboardReceiver.transmit (); Js._false)) Js._false)
