715a942e66e6909556d98b1d5262ef1a7e8c88e3
jaguillo <juloo.dsi@gmail.com>
2016-06-08 23:02:18 +0200

    Complete a bit Chrome_tabs

--- srcs/chrome_api/Chrome_bookmarks.mli
+++ srcs/chrome_api/Chrome_bookmarks.mli
@@ -6,7 +6,7 @@
 (*   By: juloo <juloo@student.42.fr>                +#+  +:+       +#+        *)
 (*                                                +#+#+#+#+#+   +#+           *)
 (*   Created: 2016/05/15 16:08:09 by juloo             #+#    #+#             *)
-(*   Updated: 2016/06/07 22:57:23 by juloo            ###   ########.fr       *)
+(*   Updated: 2016/06/08 00:38:07 by juloo            ###   ########.fr       *)
 (*                                                                            *)
 (* ************************************************************************** *)
 
@@ -14,11 +14,11 @@ class type tree_node = object
 
 	method id			:Js.js_string Js.t Js.readonly_prop
 	method parentId		:Js.js_string Js.t Js.optdef Js.readonly_prop
-	method index		:int Js.t Js.optdef Js.readonly_prop
+	method index		:int Js.optdef Js.readonly_prop
 	method url			:Js.js_string Js.t Js.optdef Js.readonly_prop
 	method title		:Js.js_string Js.t Js.readonly_prop
-	method dateAdded	:float Js.t Js.readonly_prop
-	method dateGroupModified	:float Js.t Js.optdef Js.readonly_prop
+	method dateAdded	:Js.number Js.t Js.readonly_prop
+	method dateGroupModified	:Js.number Js.t Js.optdef Js.readonly_prop
 	method children		:tree_node Js.t Js.js_array Js.t Js.optdef Js.readonly_prop
 
 end
--- srcs/chrome_api/Chrome_tabs.mli
+++ srcs/chrome_api/Chrome_tabs.mli
@@ -6,17 +6,27 @@
 (*   By: juloo <juloo@student.42.fr>                +#+  +:+       +#+        *)
 (*                                                +#+#+#+#+#+   +#+           *)
 (*   Created: 2016/06/07 22:56:36 by juloo             #+#    #+#             *)
-(*   Updated: 2016/06/07 23:27:39 by juloo            ###   ########.fr       *)
+(*   Updated: 2016/06/08 22:58:21 by juloo            ###   ########.fr       *)
 (*                                                                            *)
 (* ************************************************************************** *)
 
+type muted_info_reason = Js.js_string
+
+class type muted_info = object
+
+	method muted		:bool Js.t Js.readonly_prop
+	method reason		:muted_info_reason Js.t Js.readonly_prop
+	method extensionId	:Js.js_string Js.t Js.optdef Js.readonly_prop
+
+end
+
 class type tab = object
 
-	method id			:int Js.t Js.optdef Js.readonly_prop
+	method id			:int Js.optdef Js.readonly_prop
 
-	method index		:int Js.t Js.readonly_prop
-	method windowId		:int Js.t Js.readonly_prop
-	method openerTabId	:int Js.t Js.optdef Js.readonly_prop
+	method index		:int Js.readonly_prop
+	method windowId		:int Js.readonly_prop
+	method openerTabId	:int Js.optdef Js.readonly_prop
 
 	method selected		:bool Js.t Js.readonly_prop
 	method highlighted	:bool Js.t Js.readonly_prop
@@ -30,51 +40,94 @@ class type tab = object
 	method status		:Js.js_string Js.t Js.optdef Js.readonly_prop
 
 	method incognito	:bool Js.t Js.readonly_prop
-	method width		:int Js.t Js.optdef Js.readonly_prop
-	method height		:int Js.t Js.optdef Js.readonly_prop
+	method width		:int Js.optdef Js.readonly_prop
+	method height		:int Js.optdef Js.readonly_prop
 	method sessionId	:Js.js_string Js.t Js.optdef Js.readonly_prop
 
-	(* TODO: mutedInfo *)
+	method mutedInfo	:muted_info Js.t Js.optdef Js.readonly_prop
 
 end
 
+type zoom_settings_mode = Js.js_string
+type zoom_settings_scope = Js.js_string
+
+class type zoom_settings = object
+
+	method mode			:zoom_settings_mode Js.t Js.optdef Js.readonly_prop
+	method scope		:zoom_settings_scope Js.t Js.optdef Js.readonly_prop
+	method defaultZoomFactor	:Js.number Js.t Js.optdef Js.readonly_prop
+
+end
+
+(* properties/query objects are not typed *)
 class type t = object
 
-	method get				:int Js.t -> (tab Js.t -> unit) -> unit Js.meth
+	method get				:int -> (tab Js.t -> unit) -> unit Js.meth
 	method getCurrent		:(tab Js.t -> unit) -> unit Js.meth
+
+	method getSelected_current	:(tab Js.t -> unit) -> unit Js.meth
+	method getSelected		:int -> (tab Js.t -> unit) -> unit Js.meth
+	method getAllInWindow_current	:(tab Js.t Js.js_array Js.t -> unit) -> unit Js.meth
+	method getAllInWindow	:int -> (tab Js.t Js.js_array Js.t -> unit) -> unit Js.meth
+
+	method duplicate		:int -> (tab Js.t Js.optdef -> unit) -> unit Js.meth
+
+	method create			:< .. > Js.t -> (tab Js.t -> unit) -> unit Js.meth
+	method query			:< .. > Js.t -> (tab Js.t Js.js_array Js.t -> unit) -> unit Js.meth
+
+	method update_current	:< .. > Js.t -> (tab Js.t Js.optdef -> unit) -> unit Js.meth
+	method update			:int -> < .. > Js.t -> (tab Js.t Js.optdef -> unit) -> unit Js.meth
+
+	method move_array		:int Js.js_array Js.t -> < .. > Js.t -> (tab Js.t Js.js_array Js.t -> unit) -> unit Js.meth
+	method move				:int -> < .. > Js.t -> (tab Js.t -> unit) -> unit Js.meth
+
+	method reload_current	:< .. > Js.t -> (unit -> unit) -> unit Js.meth
+	method reload			:int -> < .. > Js.t -> (unit -> unit) -> unit Js.meth
+
+	method remove_array		:int Js.js_array Js.t -> (unit -> unit) -> unit Js.meth
+	method remove			:int -> (unit -> unit) -> unit Js.meth
+
+	method detectLanguage_current	:(Js.js_string Js.t -> unit) -> unit Js.meth
+	method detectLanguage	:int -> (Js.js_string Js.t -> unit) -> unit Js.meth
+
+	method captureVisibleTab_current	:< .. > Js.t -> (Js.js_string Js.t -> unit) -> unit Js.meth
+	method captureVisibleTab	:int -> < .. > Js.t -> (Js.js_string Js.t -> unit) -> unit Js.meth
+
+	method executeScript_current	:< .. > Js.t -> (Js.Unsafe.any Js.js_array Js.t -> unit) -> unit Js.meth
+	method executeScript	:int -> < .. > Js.t -> (Js.Unsafe.any Js.js_array Js.t -> unit) -> unit Js.meth
+
+	method insertCSS_current	:< .. > Js.t -> (unit -> unit) -> unit Js.meth
+	method insertCSS		:int -> < .. > Js.t -> (unit -> unit) -> unit Js.meth
+
+	method setZoom_current	:Js.number Js.t -> (unit -> unit) -> unit Js.meth
+	method setZoom			:int -> Js.number Js.t -> (unit -> unit) -> unit Js.meth
+	method getZoom_current	:(Js.number Js.t -> unit) -> unit Js.meth
+	method getZoom			:int -> (Js.number Js.t -> unit) -> unit Js.meth
 
 	(* TODO: connect *)
 	(* TODO: sendRequest *)
 	(* TODO: sendMessage *)
 
-	method getSelected		:int Js.t -> (tab Js.t -> unit) -> unit Js.meth
-	method getAllInWindow	:int Js.t -> (tab Js.t Js.js_array Js.t -> unit) -> unit Js.meth
+	(* TODO: highlight callback *)
+	method highlight		:< .. > Js.t -> unit Js.meth
 
-	(* TODO: create *)
+	method setZoomSettings_current	:zoom_settings Js.t -> (unit -> unit) -> unit Js.meth
+	method setZoomSettings	:int -> zoom_settings Js.t -> (unit -> unit) -> unit Js.meth
+	method getZoomSettings_current	:(zoom_settings Js.t -> unit) -> unit Js.meth
+	method getZoomSettings	:int -> (zoom_settings Js.t -> unit) -> unit Js.meth
 
-	method duplicate		:int Js.t -> (tab Js.t -> unit) -> unit Js.meth
-
-	(* TODO: query *)
-
-	(* TODO: highlight *)
-	(* TODO: update *)
-
-	(* TODO: move *)
-	(* TODO: reload *)
-
-	method remove			:int Js.t -> (unit -> unit) -> unit Js.meth
-	(* TODO: remove<array of tab> *)
-
-	method detectLanguage	:int Js.t -> (Js.js_string Js.t -> unit) -> unit Js.meth
-
-	(* TODO: captureVisibleTab *)
-	(* TODO: executeScript *)
-	(* TODO: insertCSS *)
-
-	method setZoom			:int Js.t -> float Js.t -> (unit -> unit) -> unit Js.meth
-	method getZoom			:int Js.t -> (float Js.t -> unit) -> unit Js.meth
-
-	(* TODO: setZoomSettings *)
-	(* TODO: getZoomSettings *)
+	(* TODO: onCreated *)
+	(* TODO: onUpdated *)
+	(* TODO: onMoved *)
+	(* TODO: onSelectionChanged *)
+	(* TODO: onActiveChanged *)
+	(* TODO: onActivated *)
+	(* TODO: onHighlightChanged *)
+	(* TODO: onHighlighted *)
+	(* TODO: onDetached *)
+	(* TODO: onAttached *)
+	(* TODO: onRemoved *)
+	(* TODO: onReplaced *)
+	(* TODO: onZoomChange *)
 
 end
